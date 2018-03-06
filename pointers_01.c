1774b0096361f5024be1f007cb4120f2b4ca4b3e
Jevarom <ni.moravej@gmail.com>
2018-03-06 01:25:52 -0800

    Coorect pointer_01.c

--- pointers_01.c
+++ pointers_01.c
@@ -8,7 +8,7 @@ int main(void) {
   int_pointer = &count;
   /*Address operator has the effect of assigning to the variable int_pointer,
   the value of count, but a pointer to the variable count*/
-  x = *int_pointer //Reference the contents of 'count' through the pointer
+  x = *int_pointer; //Reference the contents of 'count' through the pointer
   //variable 'int_pointer', you use the indirection operator, which is the *
 
   printf ("count = %i, x = %i\n", count, x);
