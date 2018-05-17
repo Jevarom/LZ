7a4cf09131ecf7dde60feebc1dd9af44fed09f02
Jevarom <ni.moravej@gmail.com>
2018-05-17 00:31:12 -0700

    Completed struct_06.c

--- struct_06.c
+++ struct_06.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 struct time {
   int hour;
@@ -9,7 +8,38 @@ struct time {
 
 int main(void) {
   struct time timeUpdate (struct time now);
-  struct time currentTime, nextTime;
+  struct time testTimes[5] = {{11, 59, 59}, {12, 0, 0}, {1, 29, 59}, {23, 59, 59}
+  , {19, 12, 27}};
+  int i;
 
-  printf ("Enter the time (hh:mm:ss): ");
-  scanf("%i:%i:%i", &currentTime.hour, &currentTime.minutes, &currentTime.seconds);
+  for(i = 0; i < 5; ++i) {
+    printf("Time is %.2i:%.2i:%.2i", testTimes[i].hour, testTimes[i].minutes, testTimes[i].seconds);
+
+    testTimes[i] = timeUpdate(testTimes[i]);
+
+    printf("...one second later it's %.2i:%.2i:%.2i\n", testTimes[i].hour, testTimes[i].minutes, testTimes[i].seconds);
+  }
+
+  return 0;
+
+}
+
+//TimeUpdate function
+
+  struct time timeUpdate(struct time now) {
+    ++now.seconds;
+
+    if (now.seconds == 60) {
+      now.seconds = 0;
+      ++now.minutes;
+
+      if (now.minutes == 60) {
+        now.minutes = 0;
+        ++now.hour;
+
+        if (now.hour == 24)
+          now.hour = 0;
+      }
+    }
+    return now;
+  }
