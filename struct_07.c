c0cd484deaf8e5ff9775e94651a5b800eaf67887
Jevarom <ni.moravej@gmail.com>
2018-07-08 09:52:42 -0700

    Completed functions 05 & 06

---
+++ struct_07.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+
+  int i;
+
+  struct month
+  {
+    int numberfDays;
+    char name[3];
+  };
+
+  const struct month months[12] = 
+
+
+
+
+
+  return 0;
+}
